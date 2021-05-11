3c9dd059b02e4ab89e790a6bf761e4c44fa68e34
abdelkader <kader3949@gmail.com>
2021-05-11 23:48:53 +0100

    ready to push to heuroko

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from database import Todo
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -15,6 +14,7 @@ class Todo(db.Model):
     
     
     def __repr__(self):
+        
         return f"task number is {self.id}"
     
       
